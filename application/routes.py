8b87c75f5efee18da6bcd63db21a5afe3c325702
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-20 18:12:20 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -10,14 +10,17 @@ def index():
 @app.route("/cavern")
 def cavern():
     cavern_seed = random.randrange(1000,9999)*1000
-    cavern_map = []
+    cavern_map2 = []
     random.seed(cavern_seed)
     cavern_map = [0 for _ in range (random.randrange(6,11))]
     cavern_map.extend([1 for _ in range (5, 10)])
     cavern_map.extend([0 for _ in range (6, 10)])
     print (cavern_map)
+    cavern_map2.append(cavern_map)
+    '''
     cavern_map = [
         [0,1,1,0,0],
         [0,1,1,1,0]
     ]
-    return render_template("cavern.html", map=cavern_map, nav_cavern="active")
+    '''
+    return render_template("cavern.html", map=cavern_map2, nav_cavern="active")
