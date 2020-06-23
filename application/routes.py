89d76ccc830974519ff4df53ffc3e5074ef5f4e2
LeoKnox <LeoKnox@users.noreply.github.com>
2020-06-23 08:31:40 -0700

    Add files via upload

--- application/routes.py
+++ application/routes.py
@@ -11,5 +11,20 @@ def index():
 def cavern():
     cavern_seed = random.randrange(1000,9999)*1000
     cavern_map = ['00000001111110000000']
-    print(cavern_map[0].index('1'))
+    j = '1'
+    walls = []
+    i = 0
+    while i < 20 and cavern_map[0].find(j) != -1:
+        if cavern_map[0].find(j) == -1:
+            break;
+        t = cavern_map[0].index(j, i)
+        walls.append(t)
+        print(walls)
+        i = t
+        if j == '1':
+            j = '0'
+        else:
+            j = '1'
+        i += 1
+    print(walls)
     return render_template("cavern.html", map=cavern_map, nav_cavern="active")
