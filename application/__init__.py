bd6e70e8dc5d74050a4ff0c731fa1f23b9d9a39e
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-03 21:55:30 -0700

    Add files via upload

--- application/__init__.py
+++ application/__init__.py
@@ -1,5 +1,14 @@
 from flask import Flask
+import mysql.connector
 
 app = Flask(__name__)
+app.secret_key = 'random_string'
+
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="root",
+    database="kanji_app_db"
+)
 
 from application import routes
--- application/routes.py
+++ application/routes.py
@@ -1,133 +1,80 @@
-from application import app
-from flask import render_template
-import random
+from application import app, mydb
+from flask import render_template, redirect, request, session
+import json, random, math
+
+mycursor = mydb.cursor(buffered=True)
+mc = mydb.cursor()
 
 @app.route("/")
-@app.route("/index")
+@app.route("/index", methods=["GET", "POST"])
 def index():
-     j = '1'
-    walls = []
-    i = 0
-    while i < 20 or cavern_map[0].count(j, i):
-        i = cavern_map[0].find(j, i)
-        if i == -1:
-            break
-        walls.append(i)
-        if j == '0':
-            j = '1'
-        else:
-            j = '0'
-    cavern_map = ['00011111100011110000']
-    return render_template("index.html", nav_index="active")
+    x = "SELECT DISTINCT grade, COUNT(*) AS total FROM kanji_app_db.kanji_dict GROUP BY grade"
+    mycursor.execute(x)
+    myresult = mycursor.fetchall()
+    kanjinumber = sum(i[1] for i in myresult)
+    if request.method == 'POST':
+        session['grades'] = request.form.getlist('grades')
+    return render_template("index.html", nav_index="active", myresult=myresult, kanjinumber=kanjinumber)
 
-@app.route("/cavern")
-def cavern():
-    cavern_seed = random.randrange(1000,9999)*1000
-    cavern_map = ['00000001111110000000']
-    j = '1'
-    walls = []
-    i = 0
-    while i < 20 or cavern_map[0].count(j, i):
-        i = cavern_map[0].find(j, i)
-        if i == -1:
-            break
-        walls.append(i)
-        if j == '0':
-            j = '1'
-        else:
-            j = '0'
-        i += 1
-    for x in range(15):
-        r = random.randrange(1,4)
-        cavern_map2 = list(cavern_map[x])
-        if r == 1:
-            if random.randrange(0,3):
-                cavern_map2[walls[0]-1] = '1'
-                walls[0] -= 1
-            else:
-                cavern_map2[walls[0]+1] = '0'
-                walls[0] += 1
-        elif r == 2:
-            if random.randrange(0,3):
-                cavern_map2[walls[1]] = '1'
-                walls[1] += 1
-            else:
-                cavern_map2[walls[1]-1] = '0'
-                walls[1] -= 1
-        cavern_map2 = "".join(cavern_map2)
-        cavern_map.append(cavern_map2)
-        print (cavern_map)
-    return render_template("cavern.html", map=cavern_map, nav_cavern="active")
+@app.route("/practice")
+def practice():
+    if 'grades' in session:
+        grades = session['grades']
+    else:
+        grades=[1]
+    b = "SELECT * FROM kanji_app_db.kanji_dict WHERE "
+    for i in grades:
+        b += "grade = " + str(i) + " OR "
+    b = b[:-3]
+    mycursor.execute(b)
+    mykanji = mycursor.fetchall()
+    jkanji = json.dumps(mykanji)
+    introkanji = mykanji[0]
+    return render_template("practice.html", nav_practice="active", mykanji=jkanji, introkanji=introkanji)
 
-@app.route("/cavern2")
-def cavern2():
-    return render_template("cavern2.html", map=cavern_map, nav_cavern2="active")
+@app.route("/quiz")
+@app.route("/quiz/<timer>")
+def quiz(timer=7):
+    if 'grades' in session and session['grades']!=[]:
+        grades = session['grades']
+    else:
+        grades=[1]
+    print (grades)
+    newsql = "SELECT * FROM kanji_dict WHERE "
+    for i in grades:
+        newsql += " grade = " + str(i)
+        if i != grades[len(grades)-1]:
+            newsql += " OR "
+    x = newsql.replace("*", "count(*)")
+    print(x)
+    mycursor.execute(x)
+    quiz_kanji = mycursor.fetchone()
+    kanji_id = math.floor(random.random()*quiz_kanji[0])
+    sql = "SELECT * FROM kanji_dict WHERE idkanji_dict = " + str(kanji_id)
+    mycursor.execute(sql)
+    quiz_kanji = mycursor.fetchone()
+    return render_template("quiz.html", nav_quiz="active", quiz_kanji=quiz_kanji, timer=timer)
 
+@app.route("/about")
+def about():
+    return render_template("about.html", nav_about="active")
 
-@app.route("/cavern4")
-def cavern4():
-    cavern_seed = random.randrange(1000,9999)*1000
-    cavern_map = ['00000001111110000000']
-    cavern_map = ['00011111100011110000']
-    #cavern_map = ['01011011100011110010']
-    '''
-    cavern_row = cavern_map[0].split('0')
-    cavern_row = [i for i in cavern_row if i]
-    print(cavern_row)
-    '''
-    m='1'
-    n='0'
-    cnt = 0
-    pnts = []
-    for i in cavern_map[0]:
-        if i == n:
-            cnt += 1
-        else:
-            pnts.append(cnt)
-            cnt = 1
-            m,n = n,m
-    pnts.append(cnt)
-    for l in range(1, 15):
-        for i in range(1,len(pnts),2):
-            x = random.randint(0,4)
-            if x == 1:
-                pnts[i-1] -= 1
-                pnts[i] += 1
-    print(pnts)
-        b = '0'
-        li = ['0', '1', '2', '3']
-        x = random.choices(li, k=8)
-        print (x)
-        for i in pnts:
-            new_row = ""
-            new_row += b*i
-            if b == '0':
-                b = '1'
-            else:
-                b = '0'
-        print(new_row)
-        cavern_map.append(new_row)
-    return render_template("cavern2.html", map=cavern_map, nav_cavern2="active")
+@app.route("/test")
+def test():
+    x = "SELECT DISTINCT grade, COUNT(*) AS total FROM kanji_app_db.kanji_dict GROUP BY grade"
+    if 'grades' in session:
+        grades = session['grades']
+    else:
+        grades=[1]
+    b = "SELECT * FROM kanji_app_db.kanji_dict WHERE "
+    for i in test_list:
+        b += "grade = " + str(i) + " OR "
+    b = b[:-3]
+    b += "ORDER BY RAND() LIMIT 1;"
+    mycursor.execute(b)
+    test_data = mycursor.fetchall()
+    return render_template("test.html", test_data=test_data, grades = grades)
 
-@app.route("/cavern3")
-def cavern3():
-    cavern_seed = random.randrange(1000,9999)*1000
-    cavern_map = ['00000001111110000000']
-    cavern_map = ['00011111100011110000']
-    cavern_row = []
-    test = '0'
-    temp = ''
-    for i in range(len(cavern_map[0])):
-        print("###" + str(i))
-        if cavern_map[0][i] == test:
-            temp += test
-        else:
-            cavern_row.append(i)
-            temp = '1' if temp == '0' else '0'
-    cavern_row.append(temp)
-    print(cavern_row)
-    paths = random.randrange(0, 2**(len(cavern_row)//2)+1)
-    print(paths)
-    #for j in cavern_row:
-    #    print(j)
-    return render_template("cavern2.html", map=cavern_map, nav_cavern2="active")
+@app.route("/linked")
+def linked():
+    return redirect("/test")
