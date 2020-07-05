bb2b553c8d63ec879e7d066c0cd548869145203a
LeoKnox <LeoKnox@users.noreply.github.com>
2020-07-05 01:08:02 -0700

    Add files via upload

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,5 @@
 from flask import Flask
-import mysql.connector
 
 app = Flask(__name__)
-app.secret_key = 'random_string'
-
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    passwd="root",
-    database="kanji_app_db"
-)
 
 from application import routes
